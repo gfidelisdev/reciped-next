9858b23db7397ed55e51ae80fcfb7fe61f25226a
gfidelisdev <109247627+gfidelisdev@users.noreply.github.com>
2022-07-25 10:09:18 -0300

    visual

--- components/CardArea/Card/index.jsx
+++ components/CardArea/Card/index.jsx
@@ -4,9 +4,9 @@ export default function Card(props) {
     let theme = props.theme||''
     console.log(styles)
   return (
-    <div className={`flex flex-col m-3`}>
-        <div className={styles["card-picture"]}></div>
-        <div className={`card-header${theme}`}>
+    <div className={`flex flex-col m-3 rounded-lg shadow-lg drop-shadow-lg hover:scale-110 bg-slate-600 ${styles["card-main"]}`}>
+        <div className={`${styles["card-picture"]}`}></div>
+        <div className={`${styles["card-header"]}`}>
             <h2>Header</h2>
         </div>
         <div className={`card-body${theme}`}>
--- components/CardArea/index.jsx
+++ components/CardArea/index.jsx
@@ -5,9 +5,16 @@ export default function CardArea(props) {
   return (
     <div className="my-4">
       <div className="flex flex-wrap justify-center">
-        <h1>{props.title}</h1>
+        <h1 className="text-4xl">{props.title}</h1>
       </div>
-      <div className="flex">
+      <div className="flex flex-wrap">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
         <Card />
         <Card />
