25ddc3d8b05b3715de6578f73561f5e18634bd46
Guilherme <guilherme.silva@mpt.mp.br>
2022-07-29 14:41:09 -0300

    card changes

--- components/CardArea/Card/index.jsx
+++ components/CardArea/Card/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import styles from './card.module.css'
+
+const nopic = require('../../../defaults/pictures')
+
 export default function Card(props) {
     let theme = props.theme||''
     console.log(styles)
+    let picture = props.recipe?.picture || nopic.picture
   return (
-    <div className={`flex flex-col m-3 rounded-lg shadow-lg drop-shadow-lg hover:scale-110 bg-slate-600 ${styles["card-main"]}`}>
-        <div className={`${styles["card-picture"]}`}></div>
+    <div className={`flex flex-col m-3 rounded-lg shadow-2xl drop-shadow-2xl hover:scale-110 bg-slate-600 ${styles["card-main"]}`}>
+        <div className={`${styles["card-picture"]}`}>
+            <img src={picture} alt="" srcset="" />
+        </div>
         <div className={`${styles["card-header"]}`}>
             <h2>Header</h2>
         </div>
-        <div className={`card-body${theme}`}>
+        <hr></hr>
+        <div className={`${styles["card-body"]}`}>
             Corpo
         </div>
-        <div className={`card-footer${theme}`}>
+        <hr></hr>
+        <div className={`${styles["card-footer"]}`}>
             Pé
         </div>
     </div>
---
+++ defaults/pictures.js
@@ -0,0 +1,3 @@
+module.exports = {
+    picture: 'https://loremflickr.com/320/240/food'
+}
